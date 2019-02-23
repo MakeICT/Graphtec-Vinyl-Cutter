4c29a4c9b4a944d2e883f82ab8578268baf8a0af
Christian Kindel <iceman81292@gmail.com>
2019-02-22 21:30:05 -0600

    add experimental functions to stop, pause, and resume serial transfer

--- vinylcut/graphtec.py
+++ vinylcut/graphtec.py
@@ -3,10 +3,24 @@
 import serial, os
 
 class Graphtec():
+    def __init__(self):
+        return
+        self.s = serial.Serial("/dev/ttyS0", 9600, stopbits=serial.STOPBITS_TWO)
+
     def Run(self, filename):
+        return
         print(os.path)
         # cut_file = os.listdir('./CUT_NOW')[0]
-        s = serial.Serial("/dev/ttyS0", 9600, stopbits=serial.STOPBITS_TWO)
         s.write(open(filename,"r").read())
         s.close()
         # os.rename("./CUT_NOW/"+cut_file, "./CUT_DONE/"+cut_file)
+
+    def Stop(self):
+        self.s.reset_output_buffer()
+        s.close()
+
+    def Pause(self):
+        self.s.set_output_flow_control(True)
+
+    def UnPause(self):
+        self.s.set_output_flow_control(False)
